12ee841fae9bd466328bc6dfc8ac91c3c26c6498
marceloarauj <42809145+marceloarauj@users.noreply.github.com>
2019-05-16 06:29:21 -0300

    Add files via upload

---
+++ AplicacaoWebExemplo/AplicacaoWebExemplo/Controllers/GithubController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AplicacaoWebExemplo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AplicacaoWebExemplo.Controllers
+{   
+    //[Route("apirest")]
+    public class GithubController : Controller
+    {
+        // GET: Github
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Github/Repositorios/5
+        [HttpGet("repositorios/{ident?}")]
+        public ActionResult Repositorios(string ident)
+        {
+            Repositorio rep = new Repositorio { Id = ident };
+            ViewBag.Repositorio = rep;
+            
+            return View();
+        }
+
+        // GET: Github/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Github/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Github/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Github/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Github/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Github/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AplicacaoWebExemplo/AplicacaoWebExemplo/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code
+
+$(document).ready(function () {
+
+    var usuario = $('#chave').html();
+    // obter Repositórios do Github
+
+        $.ajax({
+            method: 'GET',
+            dataType: "json",
+            contentType: 'application/json',
+            url: 'https://api.github.com/users/' + usuario+ '/repos',
+            success: function (response, s) { atualizarTabela(response); },
+            error: function (request,status,error) { alert(error);}
+        });
+
+        
+        
+       
+
+    function atualizarTabela(response) {
+
+        for (i = 0; i < response.length; i++){
+            $('#reps').append('<tr><td>' + response[i].name + '</td></tr>');
+        }
+        
+    }    
+    
+    
+});
+
